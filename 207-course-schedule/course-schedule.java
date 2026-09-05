8eaa51aee996d8acf8b41574d2041431d70db569
Rajroushan-Singh <singhrajroushan09@gmail.com>
2026-09-05 16:21:32 +0530

    Time: 6 ms (72.23%) | Memory: 46.9 MB (87.86%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         if(numCourses==1)return true;
+        int n=prerequisites.length;
         List<List<Integer>> g=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             g.add(new ArrayList<>());
-        }
+        }        
         int[] indegree=new int[numCourses];
-        Queue<Integer> q=new LinkedList<>();
-        for(int[] p:prerequisites){
-            int v=p[0];
-            int u=p[1];
-            g.get(u).add(v);
-            // g.get
-            indegree[v]++;
+
+        for(int[] pre:prerequisites){
+            int u=pre[0];
+            int v=pre[1];
+            g.get(v).add(u);
+            indegree[u]++;
         }
-        
+
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.add(i);
@@ -24,17 +25,13 @@ class Solution {
         while(!q.isEmpty()){
             int node=q.poll();
             c++;
-            for(int nei:g.get(node)){
-                indegree[nei]--;  
+            for(int nei:g.get(node) ){
+                indegree[nei]--;
                 if(indegree[nei]==0){
                     q.add(nei);
-                } 
-            }
-            // if(c!=numCourses){
-            //     return true;
-            // }
+                }
+            } 
         }
         return c==numCourses;
-
     }
 }
