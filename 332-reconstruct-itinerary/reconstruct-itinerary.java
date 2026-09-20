7c42d675a10876c9361bb6811c2fc14a17b3dd31
Rajroushan-Singh <singhrajroushan09@gmail.com>
2026-09-20 18:56:14 +0530

    Time: 7 ms (26.66%) | Memory: 46.8 MB (92.59%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,35 +1,28 @@
-class Solution {        
-    List<String> ans=new ArrayList<>();
-    Queue<String> q=new LinkedList<>();
-    Map<String, PriorityQueue<String> > map=new HashMap<>();
-
+class Solution {
+    public void dfs(String s,Map<String,List<String>> g,List<String> ans){
+        while(!g.get(s).isEmpty() ){
+            String to=g.get(s).remove(g.get(s).size()-1);
+            dfs(to,g,ans);
+        }
+        ans.add(s);
+    }
     public List<String> findItinerary(List<List<String>> tickets) {
-
+        Map<String,List<String>> g=new HashMap<>();
         for(List<String> l:tickets){
             String from=l.get(0);
             String to=l.get(1);
-            map.putIfAbsent(from, new PriorityQueue<>());
-            map.get(from).offer(to);
-
+            g.putIfAbsent(from,new ArrayList<>());
+            g.putIfAbsent(to,new ArrayList<>());
+            g.get(from).add(to);
         }
-        int n=map.size();
-        q.offer("JFK");
-        dfs("JFK");
-        // while(!q.isEmpty()){
-        //     String node=q.poll();
-        //     ans.add(node);
-        //     for(String nei:map.get(node)){
-        //         dfs(nei);
-        //     }
-        // }
+        for (List<String> destinations : g.values()) {
+            destinations.sort(Collections.reverseOrder());
+        }
+
+        List<String> ans=new ArrayList<>();
+        dfs("JFK",g,ans);
         Collections.reverse(ans);
         return ans;
-    }
-    public void dfs(String node){
-        while(map.containsKey(node) && !map.get(node).isEmpty() ){
-            String next=map.get(node).poll();
-            dfs(next);
-        }
-        ans.add(node);
+
     }
 }
